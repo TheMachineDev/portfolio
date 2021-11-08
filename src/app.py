2ba78eb90eeeb8527419c65ae58a6e04035750d9
TM <gustavoplm@hotmail.com>
2021-11-08 16:01:57 -0300

    requirements adicionado e port para flask concluido

--- src/app.py
+++ src/app.py
@@ -1,12 +1,32 @@
 from flask import Flask, render_template
 
+# Instanciando app
 app = Flask(__name__)
 
 
+# Rotas
+
+
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 
+@app.route('/job')
+def job():
+    return render_template('job.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/curriculum')
+def curriculum():
+    return render_template('curriculum.html')
+
+
+# Rodando o app
 if __name__ == '__main__':
     app.run(debug=True)
